92637eac68fe213c6ad084985ecef3870636d899
Ayush Kumar <ayush805126ind@gmail.com>
2026-01-07 23:57:38 +0530

    string problem 3

---
+++ longestCommomPrefix.py
@@ -0,0 +1,8 @@
+def longest_common_prefix(strs):
+    if not strs:
+        return ""
+    prefix = strs[0]
+    for s in strs[1:]:
+        while not s.startswith(prefix):
+            prefix = prefix[:-1]
+    return prefix
